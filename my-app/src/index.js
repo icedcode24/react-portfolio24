8de6a9484315aeaf0b1dd79141125d1048e0ff45
Cedric <cedricjhamilton@gmail.com>
2024-04-24 22:48:01 -0400

    updated imports in index js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import 'https://cdn.simplecss.org/simple.min.css';
+import App from './my-app';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
